427e57017c83b3f9c6c17fb9750caa5e6b74323f
michael-m-2983 <michaelmccright2983@gmail.com>
2024-02-03 12:21:22 -0600

    Improved selection menu

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,16 @@ import React from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 
 import { InputText } from "primereact/inputtext";
+import { Fieldset } from "primereact/fieldset";
+import { Button } from "primereact/button";
 
 export default function App() {
   //TODO: Make a file upload input instead of a text box.
-  return <div>
-    <InputText onChange={(e) => invoke('file_blaster', {path: e.target.value})}/>
-  </div>;
+
+  const [path, setPath] = React.useState("");
+
+  return <Fieldset legend="Select File">
+    <InputText onChange={(e) => setPath(e.target.value)}/>
+    <Button onClick={e => console.log(invoke('file_blaster', {filePath: path}))} />
+  </Fieldset>;
 }
